cb148613deb12876a0188d143a45c04ebc5b4135
Sandanov Damdin <damdin.sandanov2@bk.ru>
2025-01-25 18:17:49 +0300

    Setup Auth UI

---
+++ app/src/main/java/com/example/hhapp/EntryCodeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.hhapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.hhapp.databinding.EntryCodeFragmentBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class EntryCodeFragment : Fragment() {
+
+    private var _binding: EntryCodeFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        _binding = EntryCodeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
